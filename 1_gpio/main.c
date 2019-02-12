80a144c6e761f40ecd1b12df265f7b06ec042f1c
Oskiboy <oskar.oestby@gmail.com>
2019-02-12 08:52:18 +0100

    Initial commit

---
+++ 1_gpio/main.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+
+#define GPIO ((NRF_GPIO_REGS*)0x50000000)
+#define GPIO_OUTPUT_MASK ((1 << 0) | (0b001 << 2 )
+
+typedef struct {
+	volatile uint32_t RESERVED0[321];
+	volatile uint32_t OUT;
+	volatile uint32_t OUTSET;
+	volatile uint32_t OUTCLR;
+	volatile uint32_t IN;
+	volatile uint32_t DIR;
+	volatile uint32_t DIRSET;
+	volatile uint32_t DIRCLR;
+	volatile uint32_t RESERVED1[121];
+	volatile uint32_t PIN_CNF[32];
+} NRF_GPIO_REGS;
+
+int main(){
+	// Configure LED Matrix
+	for(int i = 4; i <= 15; i++){
+		GPIO->DIRSET = (1 << i);
+		GPIO->OUTCLR = (1 << i);
+	}
+
+	// Configure buttons
+	GPIO->PIN_CNF[26] = 0;  //pin 26 
+	GPIO->PIN_CNF[17] = 0;   //pin 17 
+
+	int sleep = 0;
+	while(1){
+
+		/* Check if button B is pressed;
+		 * turn on LED matrix if it is. */
+
+		/* Check if button A is pressed;
+		 * turn off LED matrix if it is. */
+
+		sleep = 10000;
+		while(--sleep);
+	}
+	return 0;
+}
