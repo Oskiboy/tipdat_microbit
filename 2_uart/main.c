e8526bf6fdbdfc1e099ad8d7208bade84026e796
Oskiboy <oskar.oestby@gmail.com>
2019-02-19 10:00:17 +0100

    Adds _write funciton for iprintf, and adds writing button presses

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "uart.h"
 #include "gpio.h"
 
@@ -10,11 +11,34 @@ void light_led() {
 
 
 int main(void) {
-   uart_init(); 
+   uart_init();
+   gpio_init();
+   iprintf("Norway has %d counties\n\r", 18);
+   //uart_send('A');
+   //light_led();
+   
    char msg = 'X';
    while(1) {
-        msg = uart_read();
         uart_send(msg);
+        msg = uart_read();
+        //If the button is low, send an A
+        if(gpio_read_btn(BUTTON_A_PIN)) {
+            uart_send('A');
+        }
+        if(gpio_read_btn(BUTTON_B_PIN)) {
+            uart_send('B');
+        }
+        if(msg != '\0') {
+            light_led();
+        }
         //uart_send('\n');
    }
+}
+
+ssize_t _write(int fd, const void* buf, size_t count) {
+    char* msg = (char*)(buf);
+    for(size_t i = 0; i < count; ++i) {
+        uart_send(msg[i]);
+    }
+    return count;
 }
