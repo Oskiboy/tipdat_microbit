5fe8e27f4190bb65d015c3e1d2a029f6b26f28ac
Oskiboy <oskar.oestby@gmail.com>
2019-02-19 09:39:44 +0100

    Adds GPIO functions for reading buttons

--- 2_uart/gpio.h
+++ 2_uart/gpio.h
@@ -4,6 +4,9 @@
 
 #define GPIO ((NRF_GPIO_REG*)0x50000000)
 
+#define BUTTON_A_PIN (26)
+#define BUTTON_B_PIN (17)
+
 typedef struct {
 	volatile uint32_t RESERVED0[321];
 	volatile uint32_t OUT;
@@ -16,5 +19,8 @@ typedef struct {
 	volatile uint32_t RESERVED1[120];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REG;
+
+void gpio_init();
+uint32_t gpio_read_btn(uint32_t button_pin);
 
 #endif
